6c379a956115a3e57b5dc609581d25b09c514edc
dev-kagant <gant.kerri@gmail.com>
2025-12-30 01:54:01 -0500

    add new theme, finished mobile navigation, added screen size hook

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,8 +1,11 @@
-import { NavLink, Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
+import useScreenSize from '../hooks/useScreenSize'
 import NavComponent from "./NavComponent";
+import MobileNavComponent from "./MobileNavComponent";
 
 function HeaderComponent() {
   const navigate = useNavigate();
+  const {width} = useScreenSize();
 
   const linksArray = [
     {name: 'Home', path: '/'},
@@ -12,7 +15,8 @@ function HeaderComponent() {
   return (
     <div className="flex flex-row justify-between h-12 items-center px-8 border-b-2 border-teal-800">
       <h3 className="text-4xl font-thick cursor-pointer text-teal-800" onClick={() => {navigate('/')}} >Scribble</h3>
-      <NavComponent links={linksArray}/>
+      {(width > 820) ? <NavComponent links={linksArray}/> :
+      <MobileNavComponent links={linksArray} />}
     </div>
   );
 }
---
+++ src/components/MobileNavComponent.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import { NavLink } from 'react-router';
+import { Bars3Icon } from '@heroicons/react/16/solid';
+
+function MobileNavComponent({links}) {
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  const handleActiveMenu = () => {
+    setActiveMenu(!activeMenu)
+  }
+
+  return (
+    <div className='flex flex-col w-1/2 h-screen pr-0 '>
+      <button className='h-12 absolute right-0 top-0 mr-8'><Bars3Icon className='text-teal-800 size-8 ' onClick={handleActiveMenu}/></button>
+      {activeMenu ? (
+      <nav className='flex flex-col justify-start absolute top-11.5 w-full h-auto opacity-95 text-teal-600 font-skinny text-2xl border-2 border-solid border-teal-800'>
+        {links.map((link, index) => (
+          <div key={index} className='flex p-4 border-b-2 bg-white'>
+            <NavLink
+                to={link.path}
+                className="focus:font-thick focus:text-4xl cursor-pointer text-4xl"
+            >
+              {link.name}
+            </NavLink>
+          </div>
+        ))}
+      </nav>
+        ) : (
+        null
+      )}
+    </div>
+  )
+}
+
+export default MobileNavComponent
