f324147ebf83ef01f79324cf3e739cb9dce64989
dev-kagant <gant.kerri@gmail.com>
2026-01-01 00:11:34 -0500

    remove overflow on html, add card component responsiveness

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardComponent = ({heading}) => {
   console.log('Where you at', heading);
   return (
-    <div className='flex items-center border-2 text-teal-800 w-4/5 md:w-1/2 lg:w-1/3 xl:w-1/4 m-4 xl:mx-8 h-30 md:h-36 lg:h-48 xl:h-56 py-10 rounded-2xl'>
+    <div className='flex flex-row justify-center items-center border-2 text-teal-800 w-full h-30 md:h-36 lg:h-48 xl:h-56 py-10 rounded-2xl'>
       <div className='flex justify-center items-center h-16 lg:h-24 w-full bg-teal-800 opacity-90'>
         <p className='font-thick text-2xl lg:text-4xl text-white opacity-100'>{heading}</p>
       </div>
